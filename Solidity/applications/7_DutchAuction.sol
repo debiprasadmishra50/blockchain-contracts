97d2243ccec5f682eb60f64a88aa0f7966ff95f8
debiprasadmishra50 <debiprasadmishra50@gmail.com>
2022-09-14 10:27:37 +0530

    new contracts

--- Solidity/applications/7_DutchAuction.sol
+++ Solidity/applications/7_DutchAuction.sol
@@ -9,4 +9,84 @@ interface IERC721 {
     ) external;
 }
 
-contract DutchAuction {}
+/* 
+What it is?
+    - It is a auction where seller sets a price at the start of the auction and the price goes down over time
+    - when the buyer decides the price is low enough and he buys the product, the auction ends
+*/
+
+/* 
+Deployment Steps:
+    1. Deploy the ERC721
+    2. call the mint(): account_address and nftId(any random value)
+    3. Deploy the DutchAuction
+        10000000, 1, nftAddress, nftId
+    4. approve function in NFT
+        DutchAuction Contract address, nftId
+    5. Auction has just started
+    6. execute getPrice, few times with time interval
+    7. execute buy with another address and sending ETH to it
+    8. execute the owner() in ERC721 and verify the address
+*/
+contract DutchAuction {
+    uint256 private constant DURATION = 7 days;
+
+    IERC721 public immutable nft;
+    uint256 public immutable nftId;
+
+    address payable public immutable seller;
+    uint256 public immutable startingPrice;
+
+    uint256 public immutable startedAt;
+    uint256 public immutable expiresAt;
+
+    uint256 public immutable discountRate;
+
+    constructor(
+        uint256 _startingPrice,
+        uint256 _discountRate,
+        address _nft,
+        uint256 _nftId
+    ) {
+        seller = payable(msg.sender);
+        startingPrice = _startingPrice;
+        discountRate = _discountRate;
+        startedAt = block.timestamp;
+        expiresAt = block.timestamp + DURATION;
+
+        require(
+            _startingPrice >= _discountRate * DURATION,
+            "starting price < discount"
+        ); // max amount ofdeduction applied to a starting price
+
+        nft = IERC721(_nft);
+        nftId = _nftId;
+    }
+
+    /* get current price of the NFT */
+    function getPrice() public view returns (uint256) {
+        uint256 timeElapsed = block.timestamp - startedAt;
+        uint256 discount = discountRate * timeElapsed;
+        return startingPrice - discount;
+    }
+
+    function buy() external payable {
+        // check if the auction is still live
+        require(block.timestamp < expiresAt, "auction expired");
+
+        // check if the ETH sent is > nft's current price
+        uint256 curPrice = getPrice();
+        require(msg.value >= curPrice, "ETH < price");
+
+        // transfer the nft ownership
+        nft.transferFrom(seller, msg.sender, nftId);
+
+        // refund the extra amount
+        uint256 refund = msg.value - curPrice;
+        if (refund > 0) {
+            payable(msg.sender).transfer(refund);
+        }
+
+        selfdestruct(seller); // closes the auction
+    }
+}
---
+++ Solidity/applications/8_EnglishAuction.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _nftId
+    ) external;
+}
+
+/* 
+    A seller sets a starting price and ending time, at the end of the time, the highest bidder wins
+*/
+
+/* 
+Deployment Steps
+    1. Deploy the ERC721
+    2. call mint() to owner, tokenId: 77(random)
+    3. Deploy the EnglishAuction 
+        nft address, nftId, 1
+    4. approve() of NFT, 
+        english_auction_contract, tokenId
+    5. switich accounts, call bid
+    6. after time is ended, call end() in English Auction
+*/
+contract EnglishAuction {
+    event Start();
+    event Bid(address indexed sender, uint256 amount);
+    event Withdraw(address indexed bidder, uint256 amount);
+    event End(address highestBidder, uint256 amount);
+
+    IERC721 public immutable nft;
+    uint256 public immutable nftId;
+
+    address payable public immutable seller;
+    uint32 public endAt;
+    bool public started;
+    bool public ended;
+
+    address public highestBidder;
+    uint256 public highestBid;
+    mapping(address => uint256) public bids;
+
+    constructor(
+        address _nft,
+        uint256 _nftId,
+        uint256 _startingBid
+    ) {
+        nft = IERC721(_nft);
+        nftId = _nftId;
+        seller = payable(msg.sender);
+        highestBid = _startingBid;
+    }
+
+    function start() external {
+        require(msg.sender == seller, "not seller");
+        require(!started, "started");
+
+        started = true;
+        endAt = uint32(block.timestamp + 60); // 60 secs
+        // endAt = uint32(block.timestamp + 7 days);
+        nft.transferFrom(seller, address(this), nftId);
+
+        emit Start();
+    }
+
+    function bid() external payable {
+        require(started, "not started");
+        require(block.timestamp < endAt, "");
+        require(msg.value > highestBid);
+
+        if (highestBidder != address(0)) {
+            bids[highestBidder] += highestBid;
+        }
+
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+
+        emit Bid(msg.sender, msg.value);
+    }
+
+    function withdraw() external {
+        uint256 bal = bids[msg.sender];
+
+        bids[msg.sender] = 0;
+        payable(msg.sender).transfer(bal);
+        emit Withdraw(msg.sender, bal);
+    }
+
+    function end() external {
+        require(started, "not started");
+        require(!ended, "ended");
+        require(block.timestamp >= endAt, "not ended");
+
+        ended = true;
+        if (highestBidder != address(0)) {
+            nft.transferFrom(address(this), highestBidder, nftId);
+            seller.transfer(highestBid);
+        } else {
+            nft.transferFrom(address(this), seller, nftId);
+        }
+
+        emit End(highestBidder, highestBid);
+    }
+}
