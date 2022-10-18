ef6fcaef90ced78935be2ffe7451c64e4024a0cd
debiprasadmishra50 <debiprasadmishra50@gmail.com>
2022-10-18 07:40:03 +0530

    modified and organised

--- WEB3-Practice/src/app.ts
+++ WEB3-Practice/src/app.ts
@@ -1,25 +1,55 @@
-import * as web3 from "./utils/web3";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
-(async () => {
-  web3.initWeb3();
+import Web3 from "web3";
+import * as lib from "./utils/web3";
 
-  const addresses = await web3.getAccounts();
-  const defaultAddress = addresses[0];
+let web3: Web3;
 
-  const ABI = web3.getContractABI("01_intro.sol", "Intro");
-  const CONTRACT_ADDRESS = "0x1C98637420554C7F97268B5D7bB6D954273eC0d4";
+const account = process.env.ADDRESS;
+const privateKey = process.env.PRIVATE_KEY;
+const explorerURI = "https://goerli.etherscan.io/tx";
+web3 = lib.initWeb3(`https://goerli.infura.io/v3/${process.env.INFURA_KEY}`);
+web3.eth.accounts.wallet.add(privateKey);
 
-  const contract = web3.getContractInstance(ABI, CONTRACT_ADDRESS);
+const DAI_TOKEN = "0xf2edF1c091f683E3fb452497d9a98A49cBA84666";
+import { DAI as DAI_ABI } from "../abis/DAI.json";
+const daiContract = lib.getContractInstance(DAI_ABI, DAI_TOKEN);
 
-  // Interact with contract
-  const name = await contract.methods.name().call();
-  const owner = await contract.methods.owner().call();
+const app = async () => {
+  const latestBlockNumber = await web3.eth.getBlockNumber();
+  console.log(latestBlockNumber);
 
-  console.log("[+] Name: ", name);
-  console.log("[+] Owner: ", owner);
+  // check ether balance
+  let bal = await web3.eth.getBalance(account);
+  console.log(`[+] ${account} has`, lib.fromWei(bal), "ether");
 
-  await contract.methods.setName("Debi Mishra").send({ from: defaultAddress });
+  // check DAI balance
+  let daiBalance = await daiContract.methods.balanceOf(account).call();
+  console.log(`[+] ${account} has`, daiBalance, "DAI");
 
-  const updatedName = await contract.methods.name().call();
-  console.log("[+] Updated Name: " + updatedName);
-})();
+  const to = "0x62af1bf552347b7baac5ce78790e75e19f2d77a0";
+  const amount = lib.toWei("1");
+  try {
+    const result = await daiContract.methods.transfer(to, amount).send({
+      from: account,
+      gasLimit: 6000000,
+      gasPrice: lib.toWei("1", "Gwei"),
+    });
+    console.log("[+] successful transaction:", `${explorerURI}/${result.transactionHash}`);
+  } catch (err) {
+    console.log(err);
+  }
+
+  // check ether balance
+  bal = await web3.eth.getBalance(account);
+  console.log(`[+] ${account} has`, lib.fromWei(bal), "ether");
+
+  // check DAI balance
+  daiBalance = await daiContract.methods.balanceOf(account).call();
+  console.log(`[+] ${account} has`, daiBalance, "DAI");
+};
+// freshdesk
+// e-shops
+
+app();
