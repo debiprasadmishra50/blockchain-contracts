3df10038e18100fe8a5c459380081904b19036fe
Debi Prasad Mishra <debiprasadmishra50@gmail.com>
2024-07-24 00:58:40 +0530

    Remove wallet generation/custodial_wallet_mayank from remote repository

--- wallet generation/custodial_wallet_mayank/getMnemonics.js
+++
@@ -1,9 +0,0 @@
-console.clear();
-const bip39 = require("bip39");
-
-// Generate 12 word mnemonic
-const mnemonics = {
-  DEPOSIT_MNEMONIC: bip39.generateMnemonic(),
-  WITHDRAW_MNEMONIC: bip39.generateMnemonic(),
-};
-console.log(mnemonics);
