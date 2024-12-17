a31aa65946d114216d8c2e93fb3208c8c6f1ad22
Debi Prasad Mishra <debiprasadmishra50@gmail.com>
2024-12-18 03:05:27 +0530

    updated cryptochain folder structure

---
+++ blockchain-building-from-scratch/cryptochain/app/pubsub.ts
@@ -0,0 +1,89 @@
+import { createClient } from "redis";
+import { Blockchain as ChainBlock } from "../blockchain";
+
+type Publish = {
+  channel: string;
+  message: string;
+};
+
+type Blockchain = { blockchain: ChainBlock };
+
+const CHANNELS = {
+  TEST: "TEST",
+  BLOCKCHAIN: "BLOCKCHAIN",
+};
+
+class PubSub {
+  publisher: any;
+  subscriber: any;
+  blockchain: ChainBlock;
+
+  constructor({ blockchain }: Blockchain) {
+    this.blockchain = blockchain;
+    this.publisher = createClient();
+    this.subscriber = createClient();
+
+    // this.subscriber.subscribe(CHANNELS.TEST);
+
+    // this.subscriber.on("message", (channel: string, message: string) => this.handleMessage(channel, message));
+    (async () => {
+      // Connect both clients
+      await this.publisher.connect();
+      await this.subscriber.connect();
+
+      //   // Subscribe to the channel and handle messages directly via callback
+      //   await this.subscriber.subscribe(CHANNELS.TEST, (message: string) => {
+      //     this.handleMessage(CHANNELS.TEST, message);
+      //   });
+      //   // Subscribe to the Blockchain CHannel
+      //   await this.subscriber.subscribe(CHANNELS.TEST, (message: string) => {
+      //     this.handleMessage(CHANNELS.TEST, message);
+      //   });
+
+      this.subscribeToChannels();
+    })();
+  }
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach(async (channel) => {
+      await this.subscriber.subscribe(channel, (message: string, channel: string) => {
+        this.handleMessage(channel, message);
+      });
+    });
+  }
+
+  handleMessage(channel: string, message: string) {
+    console.log(`\nMessage Received. Channel: ${channel}. Message: ${message}`);
+
+    const parsedMessage = JSON.parse(message);
+
+    if (channel == CHANNELS.BLOCKCHAIN) {
+      this.blockchain.replaceChain(parsedMessage);
+    }
+  }
+
+  publish({ channel, message }: Publish) {
+    this.subscriber
+      .unsubscribe(channel)
+      .then(() => {
+        return this.publisher.publish(channel, message);
+      })
+      .then((numClients: any) => {
+        // console.log("Published to:", numClients, "subscriber(s)");
+        return this.subscriber.subscribe(channel, () => {
+          // Do nothing on message
+        });
+      })
+      .then(() => {
+        // All done
+      });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
+  }
+}
+
+export default PubSub;
---
+++ blockchain-building-from-scratch/cryptochain/blockchain/block.ts
@@ -0,0 +1,70 @@
+import { GENESIS_DATA, MINE_RATE } from "../config";
+import { AdjustDifficulty, BlockData, MineBlockData } from "./interfaces/interfaces";
+import { binaryHash, sha256 } from "../utils/sha256-hash";
+
+class Block {
+  timestamp: number;
+  lastHash: string;
+  hash: string;
+  data: any;
+  difficulty: number;
+  nonce: number;
+
+  constructor(blockData: BlockData) {
+    this.timestamp = blockData.timestamp;
+    this.lastHash = blockData.lastHash;
+    this.hash = blockData.hash;
+    this.data = blockData.data;
+    this.nonce = blockData.nonce;
+    this.difficulty = blockData.difficulty;
+  }
+
+  static genesis(): Block {
+    return new this(GENESIS_DATA);
+  }
+
+  static mineBlock(mineBlockData: MineBlockData): Block {
+    const { lastBlock, data } = mineBlockData;
+    const lastHash = lastBlock.hash;
+
+    let hash, timestamp;
+    let { difficulty } = lastBlock;
+    let nonce = 0;
+
+    do {
+      nonce++;
+      timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp });
+      // NOTE: HEX Version of Hash
+      hash = sha256(timestamp, lastHash, data, nonce, difficulty);
+
+      // // NOTE: Binary Version of Hash
+      // hash = binaryHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== "0".repeat(difficulty));
+
+    return new this({ timestamp, lastHash, nonce, difficulty, hash, data });
+  }
+
+  static adjustDifficulty(params: AdjustDifficulty): any {
+    const { originalBlock, timestamp } = params;
+
+    const { difficulty } = originalBlock;
+
+    if (difficulty < 1) return 1;
+
+    if (timestamp - originalBlock.timestamp > MINE_RATE) return difficulty - 1;
+
+    return difficulty + 1;
+  }
+}
+
+export default Block;
+
+// const block1 = new Block({
+//   timestamp: new Date().toISOString(),
+//   lastHash: "lasthash",
+//   hash: "hash",
+//   data: "data string",
+// });
+
+// console.log("[+] Block 1", block1);
